09ab940f732010e38ce93327ce9f7d914113de3d
rahatflr <rahatflr@gmail.com>
2026-01-14 03:25:51 +0600

    Blog and Home Navigation added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,55 @@
+const noakhaliDonateBtn = document.getElementById('noakhali-donate');
+noakhaliDonateBtn.addEventListener('click', function (){
+    const noakhaliInputField = document.getElementById('noakhali-input');
+    const noakhaliInputAmountString = noakhaliInputField.value;
+    const noakhaliInputAmount = parseFloat(noakhaliInputAmountString);
+    console.log(noakhaliInputAmount);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getInputValueById(id) {
+//     return parseFloat(document.getElementById(id).value);
+
+// };
+
+// function getAmmountById(id) {
+//     return parseFloat(document.getElementById(id).innerText);
+// }
 
 
